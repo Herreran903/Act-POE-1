c7693a7c4cc95785f25da113f65545f657a9c6df
Herreran903 <89799303+Herreran903@users.noreply.github.com>
2022-11-22 20:47:06 -0500

    Update PrestamoModelo.java

--- src/modelo/PrestamoModelo.java
+++ src/modelo/PrestamoModelo.java
@@ -23,6 +23,7 @@ public class PrestamoModelo
         this.montoPrestamo = 0;
         this.cantidadMeses = 0;
         this.interesesTotales = 0;
+        aaaa
     }
     
     public PrestamoModelo(int auxMontoPrestamo, int auxCantidadMeses)
